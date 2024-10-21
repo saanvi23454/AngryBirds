191c72e8e66abdc1ab8164a5452f5f1900636fe3
Mayank Kumar <138568434+MayankK-20@users.noreply.github.com>
2024-10-22 00:13:06 +0530

    Add files via upload

---
+++ AngryBirds/lwjgl3/build/resources/main/levels/Level1/colored_land.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="colored_land" tilewidth="64" tileheight="64" spacing="2" margin="1" tilecount="225" columns="15">
+ <image source="colored_land.png" width="1024" height="1024"/>
+</tileset>
