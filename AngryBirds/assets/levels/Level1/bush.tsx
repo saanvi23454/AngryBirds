31ee458b9e0b9c8ef79887bc46b9154d03b97a52
Mayank Kumar <138568434+MayankK-20@users.noreply.github.com>
2024-10-22 10:05:22 +0530

    Deleting previous code

--- AngryBirds/assets/levels/Level1/bush.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.0" name="bush" tilewidth="64" tileheight="64" spacing="2" margin="1" tilecount="1" columns="1">
- <image source="bush.png" width="70" height="70"/>
-</tileset>
--- AngryBirds/core/src/main/java/com/MK_20/game/Sprites/Bird.java
+++
@@ -1,4 +0,0 @@
-package com.MK_20.game.Sprites;
-
-public class Bird {
-}
