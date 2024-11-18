8b563c6a3f291eeb6f07b766d99d24f1db2a97e8
saanvi23454 <160488729+saanvi23454@users.noreply.github.com>
2024-11-18 19:10:29 +0530

    Create ChickenBird.java

---
+++ AngryBirds/core/src/main/java/com/MK_20/game/Sprites/ChickenBird.java
@@ -0,0 +1,11 @@
+package com.MK_20.game.Sprites;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.physics.box2d.*;
+
+public class ChickenBird extends Bird {
+    public ChickenBird(World world, float x, float y, float width, float height, float radius) {
+        super(world,x,y, width, height, radius, new Texture("chickenBird.png"));
+        setTexture(this.bird_texture);
+    }
+}
