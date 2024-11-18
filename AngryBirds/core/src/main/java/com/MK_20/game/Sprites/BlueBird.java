71ac682ca6bfb5d8bab98ae8276df3462397dc57
saanvi23454 <160488729+saanvi23454@users.noreply.github.com>
2024-11-18 19:08:07 +0530

    Create BlueBird.java

---
+++ AngryBirds/core/src/main/java/com/MK_20/game/Sprites/BlueBird.java
@@ -0,0 +1,11 @@
+package com.MK_20.game.Sprites;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.physics.box2d.*;
+
+public class BlueBird extends Bird {
+    public BlueBird(World world, float x, float y, float width, float height, float radius) {
+        super(world,x,y, width, height, radius, new Texture("blueBird.png"));
+        setTexture(this.bird_texture);
+    }
+}
