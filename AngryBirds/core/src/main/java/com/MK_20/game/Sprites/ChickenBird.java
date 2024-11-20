74a5a0071a293ff2c9d2c77797998c429ebfcab4
saanvi23454 <160488729+saanvi23454@users.noreply.github.com>
2024-11-20 15:28:20 +0530

    Update ChickenBird.java

--- AngryBirds/core/src/main/java/com/MK_20/game/Sprites/ChickenBird.java
+++ AngryBirds/core/src/main/java/com/MK_20/game/Sprites/ChickenBird.java
@@ -4,8 +4,12 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.physics.box2d.*;
 
 public class ChickenBird extends Bird {
+
+    public ChickenBird(){}
+
     public ChickenBird(World world, float x, float y, float width, float height, float radius) {
-        super(world,x,y, width, height, radius, new Texture("chickenBird.png"));
+        super(world,x,y, width, height, radius, new Texture("chickenBird.png"), "chickenBird.png");
         setTexture(this.bird_texture);
+        setRegion(bird_texture);
     }
 }
