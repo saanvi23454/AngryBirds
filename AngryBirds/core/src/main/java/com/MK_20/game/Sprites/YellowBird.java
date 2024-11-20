c7da0a59322882e56ef679e02861ed7d9428d636
saanvi23454 <160488729+saanvi23454@users.noreply.github.com>
2024-11-20 15:30:12 +0530

    Update YellowBird.java

--- AngryBirds/core/src/main/java/com/MK_20/game/Sprites/YellowBird.java
+++ AngryBirds/core/src/main/java/com/MK_20/game/Sprites/YellowBird.java
@@ -4,8 +4,12 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.physics.box2d.*;
 
 public class YellowBird extends Bird {
+
+    public YellowBird(){}
+
     public YellowBird(World world, float x, float y, float width, float height, float radius) {
-        super(world,x,y, width, height, radius, new Texture("yellowBird.png"));
+        super(world,x,y, width, height, radius, new Texture("yellowBird.png"), "yellowBird.png");
         setTexture(this.bird_texture);
+        setRegion(bird_texture);
     }
 }
