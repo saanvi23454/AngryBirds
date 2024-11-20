1a3446cacd373b76d40b9b63cc8b3519bd873f5c
saanvi23454 <160488729+saanvi23454@users.noreply.github.com>
2024-11-20 15:29:29 +0530

    Update ToucanetBird.java

--- AngryBirds/core/src/main/java/com/MK_20/game/Sprites/ToucanetBird.java
+++ AngryBirds/core/src/main/java/com/MK_20/game/Sprites/ToucanetBird.java
@@ -4,8 +4,12 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.physics.box2d.*;
 
 public class ToucanetBird extends Bird {
+
+    public ToucanetBird(){}
+
     public ToucanetBird(World world, float x, float y, float width, float height, float radius) {
-        super(world,x,y, width, height, radius, new Texture("toucanetBird.png"));
+        super(world,x,y, width, height, radius, new Texture("toucanetBird.png"), "toucanetBird.png");
         setTexture(this.bird_texture);
+        setRegion(bird_texture);
     }
 }
