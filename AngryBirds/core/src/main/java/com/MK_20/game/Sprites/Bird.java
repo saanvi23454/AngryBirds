c527ffadcf450de00afee47dd029498943fee076
saanvi23454 <160488729+saanvi23454@users.noreply.github.com>
2024-11-21 01:22:55 +0530

    Update Bird.java

--- AngryBirds/core/src/main/java/com/MK_20/game/Sprites/Bird.java
+++ AngryBirds/core/src/main/java/com/MK_20/game/Sprites/Bird.java
@@ -18,6 +18,8 @@ public abstract class Bird extends Sprite implements Json.Serializable{
     public float x, y, width, height, radius; // Serializable fields
     public String texturePath;
 
+    public boolean isDestroyed = false;
+
     public Bird(){}
 
     public Bird(World world, float x, float y, float width, float height, float radius, Texture bird_texture, String texturePath) {
@@ -57,11 +59,6 @@ public abstract class Bird extends Sprite implements Json.Serializable{
         return new RedBird(world, b.x, b.y, b.width, b.height, b.radius);
     }
 
-    public void update(float delta) {
-        this.x=body.getPosition().x;
-        this.y=body.getPosition().y;
-        setPosition(body.getPosition().x-getWidth()/2, body.getPosition().y-getHeight()/2);
-    }
 
     public void defineBird(float x, float y, float radius) {
         BodyDef bodyDef = new BodyDef();
@@ -73,9 +70,22 @@ public abstract class Bird extends Sprite implements Json.Serializable{
         CircleShape circleShape = new CircleShape();
         circleShape.setRadius(radius);
         fixtureDef.shape = circleShape;
-        body.createFixture(fixtureDef);
+        Fixture fixture = body.createFixture(fixtureDef);
+        fixture.setUserData(this);
     }
 
+    public void update(float delta) {
+        this.x=body.getPosition().x;
+        this.y=body.getPosition().y;
+        setPosition(body.getPosition().x-getWidth()/2, body.getPosition().y-getHeight()/2);
+    }
+
+    public void dispose() {
+        if (body != null) {
+            world.destroyBody(body); // Safely remove the body from the world
+            body = null;
+        }
+    }
 
     @Override
     public void write(Json json) {
@@ -86,6 +96,7 @@ public abstract class Bird extends Sprite implements Json.Serializable{
         json.writeValue("height", height);
         json.writeValue("radius", radius);
         json.writeValue("texturePath", texturePath);
+        json.writeValue("isDestroyed", isDestroyed);
     }
 
     @Override
@@ -98,16 +109,11 @@ public abstract class Bird extends Sprite implements Json.Serializable{
             height = json.readValue("height", Float.class, jsonData);
             radius = json.readValue("radius", Float.class, jsonData);
             texturePath = json.readValue("texturePath", String.class, jsonData);
+            isDestroyed = json.readValue("isDestroyed", boolean.class, jsonData);
         }
         catch (Exception e) {
             System.out.println("Error deserializing bird: " + e.getMessage());
         }
     }
 
-    public void dispose() {
-        if (body != null) {
-            world.destroyBody(body); // Safely remove the body from the world
-            body = null;
-        }
-    }
 }
