65be535a2ab32744396a33016846debdfa00607d
saanvi23454 <160488729+saanvi23454@users.noreply.github.com>
2024-11-20 19:58:40 +0530

    Create WoodBox.java

---
+++ AngryBirds/core/src/main/java/com/MK_20/game/Sprites/WoodBox.java
@@ -0,0 +1,15 @@
+package com.MK_20.game.Sprites;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.physics.box2d.*;
+
+public class WoodBox extends Box {
+
+    public WoodBox() {}
+
+    public WoodBox(World world, float x, float y, float width, float height, float side) {
+        super(world, x, y, width, height, side, new Texture("woodBox.png"), "woodBox.png");
+        setTexture(this.boxTexture); 
+        setRegion(boxTexture);      
+    }
+}
