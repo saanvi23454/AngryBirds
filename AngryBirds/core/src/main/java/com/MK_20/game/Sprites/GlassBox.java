3936dea25ebfdda4b646662e97fc1eb44ae93080
saanvi23454 <160488729+saanvi23454@users.noreply.github.com>
2024-11-20 19:59:41 +0530

    Create GlassBox.java

---
+++ AngryBirds/core/src/main/java/com/MK_20/game/Sprites/GlassBox.java
@@ -0,0 +1,15 @@
+package com.MK_20.game.Sprites;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.physics.box2d.*;
+
+public class GlassBox extends Box {
+
+    public GlassBox() {}
+
+    public GlassBox(World world, float x, float y, float width, float height, float side) {
+        super(world, x, y, width, height, side, new Texture("glassBox.png"), "glassBox.png");
+        setTexture(this.boxTexture); 
+        setRegion(boxTexture);      
+    }
+}
