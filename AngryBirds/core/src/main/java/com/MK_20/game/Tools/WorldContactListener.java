db1c9691dcde2ed62ab6b1a88efdf84aeac8a860
saanvi23454 <160488729+saanvi23454@users.noreply.github.com>
2024-11-21 13:17:27 +0530

    all 3 collisions handled

--- AngryBirds/core/src/main/java/com/MK_20/game/Tools/WorldContactListener.java
+++ AngryBirds/core/src/main/java/com/MK_20/game/Tools/WorldContactListener.java
@@ -14,8 +14,8 @@ public class WorldContactListener implements ContactListener {
         //extracting out the fixtures that are colliding
         Fixture fixtureA = contact.getFixtureA();
         Fixture fixtureB = contact.getFixtureB();
-
-        handleCollision(fixtureA, fixtureB);
+        float relativeVelocity = calculateRelativeVelocity(contact);
+        handleCollision(fixtureA, fixtureB, relativeVelocity);
     }
 
     @Override
@@ -33,7 +33,7 @@ public class WorldContactListener implements ContactListener {
         // Called after the collision has been resolved
     }
 
-    private void handleCollision(Fixture fixtureA, Fixture fixtureB) {
+    private void handleCollision(Fixture fixtureA, Fixture fixtureB, float relativeVelocity) {
         // Check if the fixtures belong to a Bird and a Pig
         Object userDataA = fixtureA.getUserData();
         Object userDataB = fixtureB.getUserData();
@@ -44,12 +44,28 @@ public class WorldContactListener implements ContactListener {
             onBirdHitsPig((Bird) userDataB, (Pig) userDataA);
         }
 
-//        else if (userDataA instanceof Pig && userDataB instanceof Box){
-//            onBoxHitsPig((Box) userDataB, (Pig) userDataA, contact);
-//        }
-//        else if (userDataA instanceof Box && userDataB instanceof Pig){
-//            onBoxHitsPig((Box) userDataA, (Pig) userDataB, contact);
-//        }
+        else if (userDataA instanceof Pig && userDataB instanceof Box){
+            onBoxHitsPig((Box) userDataB, (Pig) userDataA, relativeVelocity);
+        }
+        else if (userDataA instanceof Box && userDataB instanceof Pig){
+            onBoxHitsPig((Box) userDataA, (Pig) userDataB, relativeVelocity);
+        }
+
+        else if (userDataA instanceof Bird && userDataB instanceof Box){
+            onBirdHitsBox((Bird) userDataA, (Box) userDataB, relativeVelocity);
+        }
+        else if (userDataB instanceof Bird && userDataA instanceof Box){
+            onBirdHitsBox((Bird) userDataB, (Box) userDataA, relativeVelocity);
+        }
+    }
+
+    private void onBirdHitsBox(Bird bird, Box box, float relativeVelocity) {
+        float boxDensity = bird.getWeight();
+        float damage = calculateDamage(relativeVelocity, boxDensity)*0.01f;
+
+        box.health -= damage;
+        if (box.health <= 0 && !box.isDestroyed){
+            box.markAsDestroyed();}
     }
 
     private void onBirdHitsPig(Bird bird, Pig pig) {
@@ -58,27 +74,26 @@ public class WorldContactListener implements ContactListener {
         pig.markAsDestroyed();
     }
 
-//    private void onBoxHitsPig(Box box, Pig pig, Contact contact){
-//        float relativeVelocity = calculateRelativeVelocity(contact);
-//        float boxDensity = box.getDensity();
-//        float damage = calculateDamage(relativeVelocity, boxDensity);
-//        pig.health -= damage;
-//        box.health -= damage;
-//        System.out.println("Pig health: " + pig.health);
-//
-//        if (pig.health <= 0 && !pig.isDestroyed) {
-//            pig.markAsDestroyed();}
-//        if (box.health <= 0 && !box.isDestroyed){
-//            box.markAsDestroyed();}
-//    }
-//
-//    private float calculateRelativeVelocity(Contact contact) {
-//        Body bodyA = contact.getFixtureA().getBody();
-//        Body bodyB = contact.getFixtureB().getBody();
-//        return bodyA.getLinearVelocity().sub(bodyB.getLinearVelocity()).len();
-//    }
-//
-//    private float calculateDamage(float velocity, float density) {
-//        return velocity * density * 0.1f;
-//    }
+    private void onBoxHitsPig(Box box, Pig pig, float relativeVelocity){
+        float boxDensity = box.getDensity();
+        float damage = calculateDamage(relativeVelocity, boxDensity);
+        pig.health -= damage;
+        box.health -= damage;
+        System.out.println("Pig health: " + pig.health);
+
+        if (pig.health <= 0 && !pig.isDestroyed) {
+            pig.markAsDestroyed();}
+        if (box.health <= 0 && !box.isDestroyed){
+            box.markAsDestroyed();}
+    }
+
+    private float calculateRelativeVelocity(Contact contact) {
+        Body bodyA = contact.getFixtureA().getBody();
+        Body bodyB = contact.getFixtureB().getBody();
+        return bodyA.getLinearVelocity().sub(bodyB.getLinearVelocity()).len();
+    }
+
+    private float calculateDamage(float velocity, float density) {
+        return velocity * density * 1f;
+    }
 }
