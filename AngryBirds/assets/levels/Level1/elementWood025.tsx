8cdab05a718256284147792c64ddfc25921a4754
saanvi23454 <160488729+saanvi23454@users.noreply.github.com>
2024-11-28 01:06:14 +0530

    deleted everything . dont worry

--- AngryBirds/assets/levels/Level1/elementWood025.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.0" name="elementWood025" tilewidth="64" tileheight="64" spacing="2" margin="1" tilecount="6" columns="2">
- <image source="elementWood025.png" width="140" height="220"/>
-</tileset>
--- AngryBirds/lwjgl3/build/resources/main/levels/Level1/blue_desert.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.0" name="blue_desert" tilewidth="64" tileheight="64" spacing="2" margin="1" tilecount="225" columns="15">
- <image source="blue_desert.png" width="1024" height="1024"/>
-</tileset>
