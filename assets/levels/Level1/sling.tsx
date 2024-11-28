973917cf9411220a99cf26aaf645bc3307e0c11d
saanvi23454 <160488729+saanvi23454@users.noreply.github.com>
2024-11-29 01:05:40 +0530

    final2

--- assets/levels/Level1/sling.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.0" name="sling" tilewidth="64" tileheight="64" tilecount="360" columns="15">
- <image source="../../sling.png" width="969" height="1580"/>
-</tileset>
